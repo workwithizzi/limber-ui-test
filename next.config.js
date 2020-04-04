531fdc63e9d1a23b61452a72a3fb065c0f31269b
Yisrael Grimes <yisraelgrimes@gmail.com>
2020-04-04 18:47:31 -0500

    added dotenv

--- next.config.js
+++ next.config.js
@@ -1,21 +1,14 @@
+require(`dotenv`).config()
 
 const withCSS = require(`@zeit/next-css`)
 const withSass = require(`@zeit/next-sass`)
 
-// module.exports = {
-// 	webpack(config) {
-// 		config.module.rules.push(
-// 			{
-// 				test: /\.ya?ml$/,
-// 				use: `js-yaml-loader`,
-// 			}
-// 		)
-// 		return config
-// 	},
-// }
-
 module.exports = withCSS(
 	withSass({
+		// ENV variables
+		env: {
+			GITHUB_PRIVATE_TOKEN: process.env.GITHUB_PRIVATE_TOKEN,
+		},
 		webpack(config) {
 			config.module.rules.push(
 				{
