57159d78292522c8b1b1a5f99c15edddff606a6d
Yisrael Grimes <yisraelgrimes@gmail.com>
2020-04-23 19:00:22 -0500

    Added Debug component and replaced double quotes w/ single

--- components/forms/Textarea.jsx
+++ components/forms/Textarea.jsx
@@ -3,16 +3,16 @@
 // Note: Does not accept children
 //
 // Example Output
-//   <label htmlFor="example">This is a textarea</label>
-//   <textarea id="example" className="pure-input-1" />
-//   <span className="pure-form-message">This is a Hint</span>
+//   <label htmlFor='example'>This is a textarea</label>
+//   <textarea id='example' className='pure-input-1' />
+//   <span className='pure-form-message'>This is a Hint</span>
 //- -----------------------------------------------------------------
 
-import React from "react"
-import PT from "prop-types"
-import stylePT from "react-style-proptype"
-
-import { debug, string } from "../../utils"
+import React from 'react'
+import PT from 'prop-types'
+import stylePT from 'react-style-proptype'
+import { Debug } from '../'
+import { string } from '../../utils'
 
 
 export class Textarea extends React.Component {
@@ -43,19 +43,32 @@ export class Textarea extends React.Component {
 				>
 					{label}
 					<textarea
-						className   = "pure-input-1"
+						className   = 'pure-input-1'
 						form        = {form}
 						id          = {id || string.camelCase(label)}
 						name        = {name || string.camelCase(label)}
 						onChange    = {this._handleChange}
 						{...props}
 					/>
-
-					{/* Debugger */}
-					{debug && debug(this)}
 				</label>
 
-				{hint && <span className="pure-form-message">{hint}</span>}
+				{hint && <span className='pure-form-message'>{hint}</span>}
+
+				{debug && (
+					<Debug
+						debug    = {debug}
+						info     = '<Textarea> Component - Props & State'
+						label    = {label}
+						// TODO: Figure this out when converting component to function
+						// _default-value={this.props.value}
+						// _current-value={this.state}
+						form     = {form || `not provided`}
+						id       = {id || string.camelCase(label)}
+						name     = {name || string.camelCase(label)}
+						onChange = {this._handleChange}
+						{...props}
+					/>
+				)}
 			</>
 		)
 	}
